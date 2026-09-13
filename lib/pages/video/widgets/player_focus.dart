81e6f9ee812a3e8748668ec70121d7b8a052e228
dom <githubaccount56556@proton.me>
2026-09-13 12:03:17 +0800

    fix #2950
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/pages/video/widgets/player_focus.dart
+++ lib/pages/video/widgets/player_focus.dart
@@ -150,18 +150,6 @@ class PlayerFocus extends StatelessWidget {
     }
 
     if (event is KeyDownEvent) {
-      final isDigit1 = key == LogicalKeyboardKey.digit1;
-      if (isDigit1 || key == LogicalKeyboardKey.digit2) {
-        if (HardwareKeyboard.instance.isShiftPressed && hasPlayer) {
-          final speed = isDigit1 ? 1.0 : 2.0;
-          if (speed != plPlayerController.playbackSpeed) {
-            plPlayerController.setPlaybackSpeed(speed);
-          }
-          SmartDialog.showToast('${speed}x播放');
-        }
-        return true;
-      }
-
       switch (key) {
         case LogicalKeyboardKey.space:
           if (plPlayerController.isLive || canPlay!()) {
@@ -240,6 +228,18 @@ class PlayerFocus extends StatelessWidget {
       }
 
       if (!plPlayerController.isLive) {
+        final isDigit1 = key == LogicalKeyboardKey.digit1;
+        if (isDigit1 || key == LogicalKeyboardKey.digit2) {
+          if (HardwareKeyboard.instance.isShiftPressed && hasPlayer) {
+            final speed = isDigit1 ? 1.0 : 2.0;
+            if (speed != plPlayerController.playbackSpeed) {
+              plPlayerController.setPlaybackSpeed(speed);
+            }
+            SmartDialog.showToast('${speed}x播放');
+          }
+          return true;
+        }
+
         switch (key) {
           case LogicalKeyboardKey.arrowLeft:
             if (hasPlayer) {
